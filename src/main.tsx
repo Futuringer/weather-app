e93877ca5220b05cdc039372b15eb3c89dca17c2
Fedorov Vladislav <futuringer.learning@gmail.com>
2024-01-06 15:54:09 +0300

    feat: remove unnecessary router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 
-import { router } from "./pages/routes";
+import WeatherPage from "./pages/weather/page";
 
 import "./common/styles/index.scss";
 
-createRoot(document.getElementById("root") as Element).render(
-  <RouterProvider router={router} />,
-);
+createRoot(document.getElementById("root") as Element).render(<WeatherPage />);
